59b0ba0ec705afd58ccbc0633f34498128d52f99
Алексей Гардер <aleksey_garder@outlook.com>
2018-11-18 19:47:22 +0700

    Add new city classes

---
+++ ALG_LAB2/City2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALG_LAB2
+{
+    class City2
+    {
+        const int stringMaxLength = 40;
+
+        public string Name { get; set; }
+
+        public int NextCityIndex { get; set; }
+
+        public void Write(BinaryWriter binaryWriter, int pointer)
+        {
+            binaryWriter.Seek(pointer, SeekOrigin.Begin);
+
+            var tempStr = new char[stringMaxLength];
+            for (int i = 0; i < Name.Length; i++)
+            {
+                tempStr[i] = Name[i];
+            }
+
+            binaryWriter.Write(tempStr);
+            binaryWriter.Write(NextCityIndex);
+        }
+
+        public void Read(BinaryReader binaryReader, int pointer)
+        {
+            binaryReader.BaseStream.Seek(pointer, SeekOrigin.Begin);
+
+            var tempString = binaryReader.ReadChars(stringMaxLength);
+
+            Name = new string(tempString);
+            NextCityIndex = binaryReader.ReadInt32();
+        }
+
+    }
+}
---
+++ ALG_LAB2/Road2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ALG_LAB2
+{
+    class Road2
+    {
+        public int CityFromPtr { get; set; }
+        public int CityToPtr { get; set; }
+        public int Distance { get; set; }
+
+        public int NextRoadIndex { get; set; }
+
+
+        public void Write(BinaryWriter binaryWriter, int pointer)
+        {
+            binaryWriter.Seek(pointer, SeekOrigin.Begin);
+            binaryWriter.Write(CityFromPtr);
+            binaryWriter.Write(CityToPtr);
+            binaryWriter.Write(Distance);
+            binaryWriter.Write(NextRoadIndex);
+        }
+
+        public void Read(BinaryReader binaryReader, int pointer)
+        {
+            binaryReader.BaseStream.Seek(pointer, SeekOrigin.Begin);
+            CityFromPtr = binaryReader.ReadInt32();
+            CityToPtr = binaryReader.ReadInt32();
+            Distance = binaryReader.ReadInt32();
+            NextRoadIndex = binaryReader.ReadInt32();
+        }
+    }
+}
